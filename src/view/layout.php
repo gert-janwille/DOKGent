d8df7e3206e1e4c4ad88800e577f56e891708969
Gert-Jan Wille <gertjanwille.w@gmail.com>
2017-01-22 18:08:51 +0100

    :construction: made layout for every page and start home page

--- src/view/layout.php
+++ src/view/layout.php
@@ -5,6 +5,24 @@
     <title>DOK</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <?php echo $css;?>
+
+    <script>
+
+    WebFontConfig = {
+      custom: {
+        families: ['Avenir'],
+        urls: ['assets/fonts/Avenir/Avenir.css']
+      }
+    };
+
+    (function(d) {
+      var wf = d.createElement('script'), s = d.scripts[0];
+      wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+      s.parentNode.insertBefore(wf, s);
+    })(document);
+
+  </script>
+
   </head>
   <body>
 
@@ -12,8 +30,35 @@
       <?php if(!empty($_SESSION['info'])): ?><div class="alert alert-success"><?php echo $_SESSION['info'];?></div><?php endif; ?>
       <?php if(!empty($_SESSION['error'])): ?><div class="alert alert-danger"><?php echo $_SESSION['error'];?></div><?php endif; ?>
 
-      <?php echo $content; ?>
+      <section class="fixed-nav">
+
+        <article class="top-nav">
+          <a href="#" class="menu icon"><span class="hidden">Menu</span></a>
+          <a href="#" class="search icon"><span class="hidden">Zoek</span></a>
+          <a href="index.php" class="logo icon"><span class="hidden">DOK</span></a>
+        </article>
+
+
+        <div class="row-container">
+          <div class="social">
+            <a href="http://www.facebook.com/pages/DOK/155427904513736" target="_blank" class="fb icon"><span class="hidden">Facebook</span></a>
+            <a href="http://twitter.com/#!/dokgent" target="_blank" class="twitter icon"><span class="hidden">Twitter</span></a>
+          </div>
+
+          <main>qsdfqsd
+            <?php if ($_GET['page'] != 'home') echo $content; ?>
+          </main>
+
+          <p class="street">Splitsing Koopvaardijlaan – Afrikalaan 9000 Gent</p>
+
+        </div>
+
+      </section>
+
+      <?php if ($_GET['page'] == 'home') echo $content; ?>
+
     </div>
+
     <?php echo $js;?>
   </body>
 </html>
